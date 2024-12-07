965ead1556e2fbad10aae863c68a69c389b85100
juodumas <juodumas@gmail.com>
2024-12-07 17:09:35 +0200

    up

--- 2024/07/07.py
+++ 2024/07/07.py
@@ -3,29 +3,26 @@ from itertools import product
 equations = [line.strip().split(": ") for line in open(0)]
 s1 = s2 = 0
 
+def calc(values, ops):
+    r = values[0]
+    for i, op in enumerate(ops):
+        if op == '|': r = int(f'{r}{values[i + 1]}')
+        elif op == '+': r += values[i + 1]
+        elif op == '*': r *= values[i + 1]
+    return r
+
 for testval, values in equations:
     testval = int(testval)
     values = list(map(int, (values.split())))
     opcount = len(values) - 1
-    ops1 = product("*+", repeat=opcount)
-    for ops in ops1:
-        r = values[0]
-        for i, op in enumerate(ops):
-            r = getattr(r, '__mul__' if op == '*' else '__add__')(values[i + 1])
-        if r == testval:
+    for ops in product("*+", repeat=opcount):
+        if calc(values, ops) == testval:
             s1 += testval
             s2 += testval
             break
     else:
-        ops2 = product("*+|", repeat=opcount)
-        for ops in ops2:
-            r = values[0]
-            for i, op in enumerate(ops):
-                if op == '|':
-                    r = int(f'{r}{values[i + 1]}')
-                else:
-                    r = getattr(r, '__mul__' if op == '*' else '__add__')(values[i + 1])
-            if r == testval:
+        for ops in product("*+|", repeat=opcount):
+            if calc(values, ops) == testval:
                 s2 += testval
                 break
 print(s1, s2)
