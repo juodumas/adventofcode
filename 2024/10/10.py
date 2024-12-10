623a6ffe74a71bf013b6bdcec1ca7a8d563fbd7a
juodumas <juodumas@gmail.com>
2024-12-10 13:43:19 +0200

    up

--- 2024/10/10.py
+++ 2024/10/10.py
@@ -1,15 +1,15 @@
 coords = {x + 1j * y: int(h) for y, ln in enumerate(open(0)) for x, h in enumerate(ln.strip())}
 
-def go(coord, height, paths=[]):
+def go(coord, height, peaks=[]):
     if height == 9:
-        paths.append(coord)
+        peaks.append(coord)
     for dir in (1 + 0j, 0 + 1j, -1 + 0j, 0 - 1j):
         if coords.get(coord + dir) == height + 1:
-            go(coord + dir, height + 1, paths)
+            go(coord + dir, height + 1, peaks)
 
-paths = {coord: [] for coord in coords}
+peaks = {coord: [] for coord in coords}
 for coord, height in coords.items():
     if height == 0:
-        go(coord, height, paths[coord])
+        go(coord, height, peaks[coord])
 
-print(sum(len(set(path)) for path in paths.values()), sum(len(path) for path in paths.values()))
+print(sum(len(set(p)) for p in peaks.values()), sum(len(p) for p in peaks.values()))
