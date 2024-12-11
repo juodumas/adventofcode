742b502d5e5cc5d80d3d100b15fb0d138cf0317f
juodumas <juodumas@gmail.com>
2024-12-11 17:39:06 +0200

    up

--- 2024/11/11.py
+++ 2024/11/11.py
@@ -4,12 +4,11 @@ from functools import cache
 def blink(stone, i):
     if i == 0:
         return 1
-    l = len(str(stone))
-    if l % 2 == 0:
+    if (l := len(str(stone))) % 2 == 0:
         m = 10 ** (l // 2)
         return blink(stone // m, i - 1) + blink(stone % m, i - 1)
     else:
-        return blink(1, i - 1) if stone == 0 else blink(stone * 2024, i - 1)
+        return blink(stone * 2024 if stone else 1, i - 1)
 
 stones = list(map(int, open(0).read().split()))
 print(sum(blink(stone, 25) for stone in stones), sum(blink(stone, 75) for stone in stones))
